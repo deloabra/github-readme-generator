8b1906bbb25518cda6f171a42c4810c18460420f
Brayden DeLoach <brayden@deloach.us>
2020-02-12 17:12:07 -0600

    Wrote the init and writeToFile functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const api = require("./utils/api.js");
 const md = require("./utils/generateMarkdown.js");
+const inquirer = require("inquirer");
+const fs = require("fs").promises;
 
 const questions = [
     {
@@ -14,7 +15,12 @@ const questions = [
     },
     {
         type: "input",
-        message: "What are the installation insctructions for you project?",
+        message: "What is the description for your project?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions for you project?",
         name: "Installation"
     },
     {
@@ -30,10 +36,19 @@ const questions = [
 ];
 
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, md.generateMarkdown(data))
+    .then(error => {
+        if(error){
+            return console.log(error);
+        }
+        console.log("Your readme has been made in the results folder.");
+    })
 }
 
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then(data => writeToFile("./results/README.md", data));
 }
 
 init();
